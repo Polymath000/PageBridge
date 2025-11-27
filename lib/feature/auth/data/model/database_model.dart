f2a13ce3b53d8789482eacfb8e7ba66497eafcee
=Polymath000 <abdalrahmankhaled642004@gmail.com>
2025-11-27 20:53:04 +0200

    feat(models): create the database model and the database entity - make some properties read only - make button property don't recieve

---
+++ lib/feature/auth/data/model/database_model.dart
@@ -0,0 +1,150 @@
+import '../../domain/entities/database_entity.dart';
+
+class DatabaseModel extends DatabaseEntity {
+  final String id;
+  const DatabaseModel({
+    required this.id,
+    required String title,
+    CoverModel? cover,
+    IconModel? icon,
+    required List<PropertyModel> properties,
+  }) : super(title: title, cover: cover, icon: icon, properties: properties);
+
+  factory DatabaseModel.fromJson(Map<String, dynamic> json) {
+    String parsedTitle = "No Title Found";
+    if (json['title'] != null && (json['title'] as List).isNotEmpty) {
+      final titleList = json['title'] as List;
+      parsedTitle = titleList.map((t) => t['plain_text'] ?? '').join();
+      if (parsedTitle.isEmpty) parsedTitle = "No Title Found";
+    }
+
+    List<PropertyModel> parsedProperties = [];
+    if (json['properties'] != null) {
+      (json['properties'] as Map<String, dynamic>).forEach((key, value) {
+        if (value['type'] != 'button') {
+          parsedProperties.add(PropertyModel.fromJson(key, value));
+        }
+      });
+    }
+
+    return DatabaseModel(
+      id: json['id'],
+      title: parsedTitle,
+      cover: json['cover'] != null ? CoverModel.fromJson(json['cover']) : null,
+      icon: json['icon'] != null ? IconModel.fromJson(json['icon']) : null,
+      properties: parsedProperties,
+    );
+  }
+}
+
+class PropertyModel extends PropertyEntity {
+  const PropertyModel({
+    required String name,
+    required String type,
+    required bool canEdit,
+    List<SelectOptionModel>? selectOptions,
+    String? formulaExpression,
+    String? relationDatabaseId,
+  }) : super(
+         name: name,
+         type: type,
+         canEdit: canEdit,
+         selectOptions: selectOptions,
+         formulaExpression: formulaExpression,
+         relationDatabaseId: relationDatabaseId,
+       );
+
+  factory PropertyModel.fromJson(String name, Map<String, dynamic> json) {
+    final type = json['type'];
+
+    bool isEditable = true;
+    const nonEditableTypes = [
+      'last_edited_time',
+      'last_edited_by',
+      'created_by',
+      'created_time',
+    ];
+    if (nonEditableTypes.contains(type)) {
+      isEditable = false;
+    }
+
+    List<SelectOptionModel>? options;
+    String? expression;
+    String? relatedDbId;
+
+    final config = json[type];
+    if (config != null && config is Map<String, dynamic>) {
+      if (['select', 'multi_select', 'status'].contains(type)) {
+        if (config['options'] != null) {
+          options = (config['options'] as List)
+              .map((e) => SelectOptionModel.fromJson(e))
+              .toList();
+        }
+      }
+
+      if (type == 'formula') {
+        expression = config['expression'];
+      }
+
+      if (type == 'relation') {
+        relatedDbId = config['database_id'];
+      }
+    }
+
+    return PropertyModel(
+      name: name,
+      type: type,
+      canEdit: isEditable,
+      selectOptions: options,
+      formulaExpression: expression,
+      relationDatabaseId: relatedDbId,
+    );
+  }
+}
+
+class SelectOptionModel extends SelectOptionEntity {
+  const SelectOptionModel({required String name, required String color})
+    : super(name: name, color: color);
+
+  factory SelectOptionModel.fromJson(Map<String, dynamic> json) {
+    return SelectOptionModel(
+      name: json['name'] ?? '',
+      color: json['color'] ?? 'default',
+    );
+  }
+}
+
+class CoverModel extends CoverEntity {
+  const CoverModel({String? type, String? url}) : super(type: type, url: url);
+
+  factory CoverModel.fromJson(Map<String, dynamic> json) {
+    final type = json['type'];
+    String? url;
+    if (type == 'external') {
+      url = json['external']['url'];
+    } else if (type == 'file') {
+      url = json['file']['url'];
+    }
+    return CoverModel(type: type, url: url);
+  }
+}
+
+class IconModel extends IconEntity {
+  const IconModel({String? type, String? emoji, String? url})
+    : super(type: type, emoji: emoji, url: url);
+
+  factory IconModel.fromJson(Map<String, dynamic> json) {
+    final type = json['type'];
+    String? emoji;
+    String? url;
+
+    if (type == 'emoji') {
+      emoji = json['emoji'];
+    } else if (type == 'external') {
+      url = json['external']['url'];
+    } else if (type == 'file') {
+      url = json['file']['url'];
+    }
+    return IconModel(type: type, emoji: emoji, url: url);
+  }
+}
